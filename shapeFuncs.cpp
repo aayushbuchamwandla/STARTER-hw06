ce9e3575ee05b497ee4bdcf5f1d427d977d6efdd
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2025-02-28 23:25:54 -0800

    Update shapeFuncs.cpp

--- shapeFuncs.cpp
+++ shapeFuncs.cpp
@@ -104,6 +104,5 @@ void initBox(struct Box *b, double ulx, double uly, double w, double h)
 
 
 double areaOfBox(Box b) {
-  return -42.0;  /* stub---make sure all tests fail initially */
-  // you can use b.width to access width, and b.height to access height
+  return b.width*b.height;
 }
