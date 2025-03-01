1383c919547c1d27e207beefbc960c1403e14d40
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2025-02-28 23:55:01 -0800

    Update utility.h

--- utility.h
+++ utility.h
@@ -6,4 +6,9 @@
 // You will need to include the function prototype in "utility.h" and
 // then be sure to  #include "utility.h" in the file where you use
 // these functions
+#ifndef UTILITY_H
+#define UTILITY_H
 
+bool approxEqual(double a, double b, double tolerance);
+
+#endif
