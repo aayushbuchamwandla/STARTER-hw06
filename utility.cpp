a94ef4770f1beac789e911790253df463ba8d621
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2025-02-28 23:54:43 -0800

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -7,4 +7,10 @@
 // You will need to include the function prototype in "utility.h" and
 // then be sure to  #include "utility.h" in the file where you use
 // these functions
+#include "utility.h"
+#include <cmath>
+using namespace std;
 
+bool approxEqual(double a, double b, double tolerance) {
+	return abs(a - b) <= tolerance;
+}
